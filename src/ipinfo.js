a82d845dd1503c414c1c87033b2116b775d031d2
Dom <dom.corvasce@yandex.com>
2017-01-13 14:58:40 +0100

    Add IPInfo module

---
+++ src/ipinfo.js
@@ -0,0 +1,37 @@
+// Access to user's location through http://ipinfo.io
+var http = require('http');
+
+function IPInfo() {
+    var ENDPOINT = 'http://ipinfo.io';
+    var coords;
+
+    /*
+     * Retrieve location's data and istantiate a new promise which
+     * can be used to access to coords when it's ready:
+     * ```
+     * var ipinfo = new IPInfo();
+     * ipinfo.get().then(function returnCoords() {
+     *   console.log(ipinfo.getCoords());
+     * });
+     * ```
+     */
+    function get() {
+        return new Promise(function getData(resolve, reject) {
+            http.get(ENDPOINT, function fetchResponse(response) {
+                response.on('data', parseData);
+                resolve();
+            });
+        });
+    }
+
+    function parseData(data) {
+        coords = JSON.parse(data);
+        return;
+    }
+
+    function getCoords() {
+        return coords;
+    }
+
+    return { get, getCoords };
+}
