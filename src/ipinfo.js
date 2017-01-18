20a58c7e3adc8d6b12e5680fc870552099df0e89
Mario Lamacchia <mario.lamacchia@nokia.com>
2017-01-17 20:15:57 -0800

    Refactor ipinfo: remove class and export function

--- src/ipinfo.js
+++ src/ipinfo.js
@@ -1,33 +1,29 @@
+/**
+ * Retrieve location's data and istantiate a new promise which
+ * can be used to access to coords when it's ready:
+ *
+ * ```
+ * var ipinfo = require('./ipinfo')
+ * ipinfo.get().then(function returnCoords() {
+ *   console.log(ipinfo.getCoords());
+ * });
+ * ```
+ */
+const http = require('http');
+
 // Access to user's location through http://ipinfo.io
-var http = require('http');
+const ENDPOINT = 'http://ipinfo.io';
 
-function IPInfo() {
-    var ENDPOINT = 'http://ipinfo.io';
-
-    /*
-     * Retrieve location's data and istantiate a new promise which
-     * can be used to access to coords when it's ready:
-     * ```
-     * var ipinfo = new IPInfo();
-     * ipinfo.get().then(function returnCoords() {
-     *   console.log(ipinfo.getCoords());
-     * });
-     * ```
-     */
-    function get() {
-        return new Promise(function getData(resolve, reject) {
-            http.get(ENDPOINT, function fetchResponse(response) {
-                response.on('data', parseData.bind(this, resolve));
-            });
-        });
-    }
-
-    function parseData(resolve, data) {
-        data = JSON.parse(data);
-        resolve(data);
-    }
-
-    return { get: get };
+function parseData(resolve, data) {
+    resolve(JSON.parse(data));
 }
 
-module.exports = new IPInfo();
+function get() {
+    return new Promise(function getData(resolve, reject) {
+        http.get(ENDPOINT, function fetchResponse(response) {
+            response.on('data', parseData.bind(this, resolve));
+        });
+    });
+}
+
+exports.get = get;
