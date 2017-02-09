2a7a00f6c3973ced72750a021372fb49e072b9ae
Dom <dom.corvasce@yandex.com>
2017-02-09 18:55:32 +0100

    Refactor OpenWeather as ES6 class

--- src/openweather.js
+++ src/openweather.js
@@ -1,78 +1,76 @@
 // Access to weather stats using OpenWeatherMap APIs.
 const http = require('http');
 
-const endpoint = 'http://api.openweathermap.org/data/2.5/weather?q={city}&apikey={api}';
-
 /*
  * This module allows you to easily retrieve weather using OpenWeather API.
  * All you need is an API key you can get here: https://openweathermap.org/api.
  *
  *   @example
- *     let weather = new OpenWeather();
- *     weather.setAPI('YOUR_API_KEY')
+ *     const weather = new OpenWeather('YOUR_API_KEY')
  *     weather.setScale('C');
  *     weather.on('New York');
  */
-function OpenWeather(apiKey, scale) {
-  this.setAPI(apiKey);
-  this.setScale(scale);
-  return this;
+class OpenWeather {
+  // Initializes a new connection with OpenWeatherMap's APIs */
+  constructor(apiKey, temperatureScale) {
+    this.apiKey = apiKey;
+    this.temperatureScale = this.checkScale(temperatureScale);
+    this.apiURL = 'http://api.openweathermap.org/data/2.5/weather?q={city}&apikey={api}';
+  }
+
+  checkScale(scale) {
+    if (!['C', 'F', 'K'].includes(scale)) {
+      throw new Error(`The given scale "${scale}" is not a valid one.`);
+    }
+
+    return scale;
+  }
+
+  // Get weather for a specific city.
+  on(city, cb) {
+    const self = this;
+    const requestURL = this.composeURL(city);
+
+    http.get(requestURL, (res) => {
+      res.on('data', self.formatWeather.bind(self, cb));
+      res.on('error', console.log);
+    });
+  }
+
+  // Format weather's stats.
+  formatWeather(cb, rawData) {
+    const data = JSON.parse(rawData);
+
+    cb({
+      min: this.convertToUserScale(data.main.temp_min),
+      max: this.convertToUserScale(data.main.temp_max),
+      weatherStatus: data.weather[0].main
+    });
+  }
+
+  // Convert temperature to the right temperature scale from Kelvin.
+  convertToUserScale(temperature) {
+    let convertedTemperature;
+
+    switch (this.temperatureScale) {
+      case 'C':
+        convertedTemperature = temperature - 273.15;
+        break;
+      case 'F':
+        convertedTemperature = (temperature * (9 / 5)) - 459.67;
+        break;
+      default:
+        convertedTemperature = temperature;
+    }
+
+    return convertedTemperature.toFixed(2);
+  }
+
+  // Compose the request's URL attaching necessary information.
+  composeURL(city) {
+    return this.apiURL.replace('{city}', city)
+                      .replace('{api}', this.apiKey);
+  }
 }
-
-OpenWeather.prototype.setAPI = function setAPI(key) {
-  this.apiKey = key;
-};
-
-OpenWeather.prototype.setScale = function setScale(givenScale) {
-  if (!['C', 'F', 'K'].includes(givenScale)) {
-    throw new Error("Invalid scale for 'setScale' , expected: C, F or K.");
-  }
-
-  this.scale = givenScale;
-};
-
-/* Get weather for a specific city. */
-OpenWeather.prototype.on = function on(city, cb) {
-  const self = this;
-  const requestURL = this.composeURL(city);
-
-  http.get(requestURL, (res) => {
-    res.on('data', self.formatWeather.bind(self, cb));
-    res.on('error', console.log);
-  });
-};
-
-OpenWeather.prototype.formatWeather = function formatWeather(cb, rawData) {
-  const data = JSON.parse(rawData);
-
-  cb({
-    min: this.convertToUserScale(data.main.temp_min),
-    max: this.convertToUserScale(data.main.temp_max),
-    weatherStatus: data.weather[0].main
-  });
-};
-
-OpenWeather.prototype.convertToUserScale = function convertToUserScale(temperature) {
-  let convertedTemperature;
-
-  switch (this.scale) {
-    case 'C':
-      convertedTemperature = temperature - 273.15;
-      break;
-    case 'F':
-      convertedTemperature = (temperature * (9 / 5)) - 459.67;
-      break;
-    default:
-      convertedTemperature = temperature;
-  }
-
-  return convertedTemperature.toFixed(2);
-};
-
-/* Compose the request's URL attaching necessary information. */
-OpenWeather.prototype.composeURL = function composeURL(city) {
-  return endpoint.replace('{city}', city)
-                 .replace('{api}', this.apiKey);
-};
 
 module.exports = OpenWeather;
--- test/openweather.spec.js
+++ test/openweather.spec.js
@@ -2,38 +2,26 @@ const expect = require('chai').expect;
 
 const OpenWeather = require('./../src/openweather');
 
-const weather = new OpenWeather('', 'C');
-
 describe('OpenWeather', function() {
-  describe('#setAPI()', function() {
-    it('should edits API key', function() {
-      weather.setAPI('MOCHA_TEST');
-      expect(weather.apiKey).to.equal('MOCHA_TEST');
-    });
+  it('constructor should accepts two parameters: API key and scale', () => {
+    expect(() => new OpenWeather('API_KEY', 'C')).to.not.throw(Error);
   });
 
-  describe('#setScale()', function() {
-    it('should raises an error if the scale is not valid', function() {
-      expect(function() {
-        weather.setScale(0);
-      }).to.throw(Error);
-    })
+  it('constructor should throws an error if one of more parameters are missing', () => {
+    expect(() => new OpenWeather('API_KEY')).to.throws(Error);
+  });
 
-    it ('should edits scale', function() {
-      weather.setScale('F');
-      expect(weather.scale).to.equal('F');
-    });
-  })
+  it('constructor should throws an error if the passed temperature scale is invalid', () => {
+    expect(() => new OpenWeather('API_KEY', 'X')).to.throw(Error);
+  });
 
-  describe('#convertToUserScale()', function() {
-    it('should convert from Kelvin to Celsius', function() {
-      weather.setScale('C');
-      expect(weather.convertToUserScale(1)).to.equal('-272.15');
-    });
+  it('convertToUserScale should converts temperature from Kelvin to Celsius', function() {
+    const weather = new OpenWeather('API_KEY', 'C');
+    expect(weather.convertToUserScale(1)).to.equal('-272.15');
+  });
 
-    it('should convert from Kelvin to Fahrenheit', function() {
-      weather.setScale('F');
-      expect(weather.convertToUserScale(1)).to.equal('-457.87');
-    })
+  it('convertToUserScale should converts temperature from Kelvin to Fahrenheit', function() {
+    const weather = new OpenWeather('API_KEY', 'F');
+    expect(weather.convertToUserScale(1)).to.equal('-457.87');
   });
 });
